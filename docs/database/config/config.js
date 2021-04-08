b98747c19f5522b56f1fc5f52e9489da1b8b92f4
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-04-08 19:31:03 -0300

    Añade base de datos

--- docs/database/config/config.js
+++ docs/database/config/config.js
@@ -1,12 +1,20 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "DB_Saldo",
-    "host": "127.0.0.1",
+    "username": "c91o82vevk1tvg7t",
+    "password": "irl9twuf8ky9cw8z",
+    "database": "gru2839oboew6ruq",
+    "host": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql",
     "root": 3306
   },
+  // "development": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "DB_Saldo",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql",
+  //   "root": 3306
+  // },
   "test": {
     "username": "root",
     "password": null,
