296270c248f51f9b15d144cfdd72f8f5afac6b08
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-04-18 14:58:39 -0300

    Añade arreglos en privacidad de datos

--- docs/app.js
+++ docs/app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var env = require('node-env-file');
-env(__dirname + '/.env');
+require('dotenv').config()
 const session = require('express-session');
 const methodOverride = require('method-override');
 
