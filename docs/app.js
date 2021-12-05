393b91274f7bde5f3b489b78a7ef4b402fdb2c0a
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-12-05 16:02:44 -0300

    Añade configuracion a mondodb

--- docs/app.js
+++ docs/app.js
@@ -1,68 +1,81 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('dotenv').config()
-const session = require('express-session');
-const methodOverride = require('method-override');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const localsUserCheck = require('./middlewares/localsUserCheck');
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/usuario');
+const localsUserCheck = require("./middlewares/localsUserCheck");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/usuario");
 
 var app = express();
 
 // view engine setup
-app.enable('trust proxy')
+app.enable("trust proxy");
 
-app.use(function(request, response, next) {
+app.use(function (request, response, next) {
   // if (process.env.NODE_ENV != 'development' && !request.secure) {
   //   return response.redirect("https://" + request.headers.host + request.url);
-  // } 
-  next(); 
-})
+  // }
+  next();
+});
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
-app.use(session({
-  secret:"Saldo", 
-  resave: true,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: "Saldo",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use(localsUserCheck);
 
-app.use('/', indexRouter);
-app.use('/admin', usersRouter);
-
+app.use("/", indexRouter);
+app.use("/admin", usersRouter);
+const url = process.env.DB_URL;
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(url, {
+    useNewUrlPArser: true,
+  })
+  .then(() => console.log("conectado!!"))
+  .catch((error) => console.log(error));
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(res.render('error',{
-    title:"Error al encontrar la página",
-    css: 'errorURL.css',
-    errorURL: 'errorURL.js'
-  }));
+app.use(function (req, res, next) {
+  next(
+    res.render("error", {
+      title: "Error al encontrar la página",
+      css: "errorURL.css",
+      errorURL: "errorURL.js",
+    })
+  );
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
