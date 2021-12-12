521663ce6df8e805752f6b1cdf98e393f7a27331
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-12-12 12:25:39 -0300

    Añade mejoras en la seleccion de los estaos y agrega fetch update del mismo

--- docs/public/javascripts/viewListClient.js
+++ docs/public/javascripts/viewListClient.js
@@ -1,21 +1,56 @@
+let url = location.protocol + "//" + location.host;
+console.log(url)
 const select = document.querySelector('#select')
 const opciones = document.querySelector('#opciones')
 const contenidoSelect = document.querySelector('#select .contenido-select ');
-const hiddentInput = document.querySelector('#inputSelect')
+const status = document.querySelector('.contenido-opcion img')
+const actionSelector = (e,id) => {
+    e.preventDefault();
 
-document.querySelectorAll('#opciones > .opcion').forEach((opcion) =>{
-    opcion.addEventListener('click', (e) =>{
-        e.preventDefault();
-        console.log("click");
-        contenidoSelect.innerHTML= e.currentTarget.innerHTML;
-        select.classList.toggle('active')
-        opciones.classList.toggle('active')
-        
-    })
-})
-select.addEventListener('click', () =>{
-    select.classList.toggle('active')//cambia la clase
+    console.log("click");
+        document.getElementById('select'+id).classList.toggle('active')
+        document.getElementById('opciones'+id).classList.toggle('active')
+}
+const updateStatus= async (status,_id)=>{
+    console.log("desde updateStatus => ",status, _id);
+    fetch(url + `/admin/update`, {
+        method: "POST",
+        body: JSON.stringify({
+          status,
+          _id
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result)
+        });
+
+}
+const actionSelected = (e,id, status,_id) => {
+    document.getElementById('select'+id).classList.toggle('active')//cambia la clase
     console.log('click en select');
+    console.log(status);//enviar por post el estatus
+    
+    updateStatus(status,_id)
+    document.getElementById('contenido-select'+id).innerHTML= e.currentTarget.innerHTML;
 
-    opciones.classList.toggle('active')//cambia la clase
+    document.getElementById('opciones'+id).classList.toggle('active')//cambia la clase
+}
+
+const modalClient = new bootstrap.Modal(document.getElementById('modalClient'))
+const on = (element, event, selector, handler) =>{
+    element.addEventListener(event, e=>{
+        if(e.target.closest(selector)){
+            handler(e)
+        }
+    })
+}
+on(document, 'click','.modalClient', e =>{
+    const fila = e.target.parentNode.parentNode
+    nombre.value = fila.children[1].innerHTML
+    modalClient.show()
 })
+
