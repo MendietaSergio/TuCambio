46bee5e520a1b0c265bb04e7864eecd4c2d3f404
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-12-12 23:24:08 -0300

    Añade funcionamiento de filtrar por mayor o menor

--- docs/public/javascripts/viewListClient.js
+++ docs/public/javascripts/viewListClient.js
@@ -1,56 +1,338 @@
 let url = location.protocol + "//" + location.host;
-console.log(url)
-const select = document.querySelector('#select')
-const opciones = document.querySelector('#opciones')
-const contenidoSelect = document.querySelector('#select .contenido-select ');
-const status = document.querySelector('.contenido-opcion img')
-const actionSelector = (e,id) => {
-    e.preventDefault();
-
-    console.log("click");
-        document.getElementById('select'+id).classList.toggle('active')
-        document.getElementById('opciones'+id).classList.toggle('active')
-}
-const updateStatus= async (status,_id)=>{
-    console.log("desde updateStatus => ",status, _id);
-    fetch(url + `/admin/update`, {
-        method: "POST",
-        body: JSON.stringify({
-          status,
-          _id
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      })
-        .then((response) => response.json())
-        .then((result) => {
-            console.log(result)
-        });
-
-}
-const actionSelected = (e,id, status,_id) => {
-    document.getElementById('select'+id).classList.toggle('active')//cambia la clase
-    console.log('click en select');
-    console.log(status);//enviar por post el estatus
-    
-    updateStatus(status,_id)
-    document.getElementById('contenido-select'+id).innerHTML= e.currentTarget.innerHTML;
-
-    document.getElementById('opciones'+id).classList.toggle('active')//cambia la clase
-}
-
-const modalClient = new bootstrap.Modal(document.getElementById('modalClient'))
-const on = (element, event, selector, handler) =>{
-    element.addEventListener(event, e=>{
-        if(e.target.closest(selector)){
-            handler(e)
-        }
+let newData;
+// window.addEventListener("DOMContentLoaded", () => {
+const ListClient = async () => {
+  await fetch(url + "/admin/list")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data fetch");
+      console.log(data);
+      mostrarData(data);
     })
-}
-on(document, 'click','.modalClient', e =>{
-    const fila = e.target.parentNode.parentNode
-    nombre.value = fila.children[1].innerHTML
-    modalClient.show()
-})
+    .catch((error) => console.log(error));
+};
+console.log(url);
+const preloader = document.querySelector("#preloader");
+ListClient();
+let loading = `<div class="preloader" ></div> 
+  <span >Cargando...</span>`;
+document.getElementById("preloader").innerHTML = loading;
+const mostrarData = (data) => {
+  newData = data;
+  let body = "";
+  let modal = "";
+  loading = `<div class="preloader" ></div> 
+  <span >Cargando...</span>`;
+  preloader.style.display = "display";
+  document.getElementById("preloader").innerHTML = loading;
+  console.log("prelaoader");
+  document.getElementById("bodyData").innerHTML = body;
+  document.getElementById("modal-container").innerHTML = modal;
+  setTimeout(function () {
+    for (let i = 0; i < data.length; i++) {
+      body += `
+            <tr>
+              <td>
+                <a
+                  class="btn btn-primary"
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalClient${data[i].orden}"
+                >
+                ${data[i].orden}
+                </a>
+              </td>
+              <td>
+                <a
+                  class="modalClient"
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalClient${data[i].orden}"
+                >
+                ${data[i].nombreCompleto}
+                </a>
+              </td>
+              <td>
+                <div class="d-flex fs-6">
+                  <div class="badge badge-sa-primary">
+                    <div class="selectbox">
+                      <div
+                        class="select"
+                        id="select${data[i].orden}"
+                        onclick="actionSelector(event, '${data[i].orden}')"
+                      >
+                        <div
+                          class="contenido-select${data[i].orden}"
+                          id="contenido-select${data[i].orden}"
+                        >
+                          <div class="contenido-opcion">
+                            <img
+                              src="../img/status/${data[i].estado}.png"
+                              alt="<% client.estado %>"
+                            />
+                          </div>
+                        </div>
+                        <i class="fas fa-chevron-down"></i>
+                      </div>
+                      <div class="opciones" id="opciones${data[i].orden}">
+                        <a
+                          href="#"
+                          class="opcion"
+                          onclick="actionSelected(event,'${data[i].orden}', 'realizado', '${data[i]._id}')"
+                        >
+                          <div class="contenido-opcion">
+                            <img
+                              src="../img/status/realizado.png"
+                              alt="realizado"
+                            />
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          class="opcion"
+                          onclick="actionSelected(event,'${data[i].orden}', 'cancelado', '${data[i]._id}')"
+                        >
+                          <div class="contenido-opcion">
+                            <img
+                              src="../img/status/cancelado.png"
+                              alt="cancelado"
+                            />
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          class="opcion"
+                          onclick="actionSelected(event,'${data[i].orden}', 'pendiente', '${data[i]._id}')"
+                        >
+                          <div class="contenido-opcion">
+                            <img
+                              src="../img/status/pendiente.png"
+                              alt="pendiente"
+                            />
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <div class="d-flex align-items-center">
+                  <div> ${data[i].created_at}</div>
+                </div>
+              </td>
+            </tr>
+            
+            `;
+      // <script src="/javascripts/actionsList.js"></script>
+      if (data[i].campoAdicional2 !== "-") {
+        modal += `<div
+            class="modal fade"
+            id="modalClient${data[i].orden}"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    Datos del cliente N°${data[i].orden}
+                  </h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                    Nombre Completo: <span class="modal-valor">
+                      <span class="modal-valor">${data[i].nombreCompleto}</span>
+                    </span>
+                  </h5>
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                    Correo personal: <span class="modal-valor">${data[i].correoElectronico}</span>
+                  </h5>
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                    Telefono: <span class="modal-valor">${data[i].telefono}</span>
+                  </h5>
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                  ${data[i].campoAdicional1} <span class="modal-valor">${data[i].campoAdicional1Valor}</span>
+                  </h5>
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                  ${data[i].campoAdicional2} <span class="modal-valor">${data[i].campoAdicional2Valor}</span>
+                  </h5>
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                  Envia: <span class="modal-valor">${data[i].envia}</span>
+                  </h5>
+                  <h5
+                    class="modal-title"
+                    id="exampleModalLabel${data[i].orden}"
+                  >
+                    Recibe:  <span class="modal-valor">${data[i].recibe}</span>
+                  </h5>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>`;
+      } else {
+        modal += `<div
+      class="modal fade"
+      id="modalClient${data[i].orden}"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              Datos del cliente N°${data[i].orden}
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <h5
+              class="modal-title"
+              id="exampleModalLabel${data[i].orden}"
+            >
+              Nombre Completo: <span class="modal-valor">
+                <span class="modal-valor">${data[i].nombreCompleto}</span>
+              </span>
+            </h5>
+            <h5
+              class="modal-title"
+              id="exampleModalLabel${data[i].orden}"
+            >
+              Correo personal: <span class="modal-valor">${data[i].correoElectronico}</span>
+            </h5>
+            <h5
+              class="modal-title"
+              id="exampleModalLabel${data[i].orden}"
+            >
+              Telefono: <span class="modal-valor">${data[i].telefono}</span>
+            </h5>
+            <h5
+              class="modal-title"
+              id="exampleModalLabel${data[i].orden}"
+            >
+            ${data[i].campoAdicional1} <span class="modal-valor">${data[i].campoAdicional1Valor}</span>
+            </h5>
+           
+            <h5
+              class="modal-title"
+              id="exampleModalLabel${data[i].orden}"
+            >
+            Envia: <span class="modal-valor">${data[i].envia}</span>
+            </h5>
+            <h5
+              class="modal-title"
+              id="exampleModalLabel${data[i].orden}"
+            >
+              Recibe:  <span class="modal-valor">${data[i].recibe}</span>
+            </h5>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+      }
+    }
 
+    preloader.style.display = "none";
+    loading = "";
+    document.getElementById("preloader").innerHTML = loading;
+    console.log("prelaoader");
+    document.getElementById("bodyData").innerHTML = body;
+    document.getElementById("modal-container").innerHTML = modal;
+  }, 2000);
+};
+// });
+const select = document.querySelector("#select");
+const opciones = document.querySelector("#opciones");
+const contenidoSelect = document.querySelector("#select .contenido-select ");
+const status = document.querySelector(".contenido-opcion img");
+const actionSelector = (e, id) => {
+  e.preventDefault();
+
+  console.log("click");
+  document.getElementById("select" + id).classList.toggle("active");
+  document.getElementById("opciones" + id).classList.toggle("active");
+};
+const updateStatus = async (status, _id) => {
+  console.log("desde updateStatus => ", status, _id);
+  fetch(url + `/admin/update`, {
+    method: "POST",
+    body: JSON.stringify({
+      status,
+      _id,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    });
+};
+const actionSelected = (e, id, status, _id) => {
+  document.getElementById("select" + id).classList.toggle("active"); //cambia la clase
+  updateStatus(status, _id);
+  document.getElementById("contenido-select" + id).innerHTML =
+  e.currentTarget.innerHTML;
+  document.getElementById("opciones" + id).classList.toggle("active"); //cambia la clase
+};
+const OrdenarMenor = async () => {
+  let dataMenor = await newData.sort(function (a, b) {
+    return a.orden - b.orden;
+  });
+  mostrarData(dataMenor);
+};
+
+const OrdenarMayor = async () => {
+  let dataMayor = await newData.sort(function (a, b) {
+    return b.orden - a.orden;
+  });
+  mostrarData(dataMayor);
+};
