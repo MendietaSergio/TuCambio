4dc9c2e51077d26cc217c7d4c5b6985c6bf3db0a
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-12-26 19:19:10 -0300

    Añade limpieza de codigo

--- docs/public/javascripts/viewListClient.js
+++ docs/public/javascripts/viewListClient.js
@@ -1,13 +1,19 @@
 let url = location.protocol + "//" + location.host;
 let newData;
+console.log("URL==> ");
 
 const ListClient = async () => {
-  await fetch(url + "/admin/list")
+  try{
+
+    await fetch(url + `/admin/list`)
     .then((response) => response.json())
     .then((data) => {
       mostrarData(data);
     })
     .catch((error) => console.log(error));
+  } catch(error){
+      console.log("Error=> ",error);
+    }
 };
 const preloader = document.querySelector("#preloader");
 ListClient();
